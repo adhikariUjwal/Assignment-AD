df9acc3fb613b0b321304bac5f9e7a7565d77298
ujwaladhikari <adhikaridujwal001@gmail.com>
2023-12-07 14:14:48 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Car
+{
+    private string brand;
+    private int year;
+    private float price;
+
+    public Car(string brand, int year, float price)
+    {
+        this.brand = brand;
+        this.year = year;
+        this.price = price;
+    }
+
+ 
+    public void DisplayCarDetails()
+    {
+        Console.WriteLine($"Brand: {brand}");
+        Console.WriteLine($"Year: {year}");
+        Console.WriteLine($"Price: {price:C}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        Car myCar = new Car("Bently", 2023, 25000000.50f);
+
+        myCar.DisplayCarDetails();
+    }
+}
