bcf4659fc81261bef4997fb3d4fb002c1a302f22
ujwaladhikari <adhikaridujwal001@gmail.com>
2023-12-07 14:59:43 +0545

    Exercise 5,6,7,8

--- Exercise4.cs
+++ Exercise4.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*using System;
 
-namespace Assignment_AD
+class Shape
 {
-    internal class Exercise4
+
+    public virtual float CalculateArea()
     {
+        return 0;
     }
 }
+
+class Circle : Shape
+{
+    private float radius;
+
+    public Circle(float radius)
+    {
+        this.radius = radius;
+    }
+
+    public override float CalculateArea()
+    {
+        return (float)(Math.PI * Math.Pow(radius, 4));
+    }
+}
+
+class Exercise5
+{
+    static void Main()
+    {
+        Circle myCircle = new Circle(8);
+
+        float area = myCircle.CalculateArea();
+        Console.WriteLine($"The area of the circle with radius 5 is: {area}");
+    }
+}
+*/
